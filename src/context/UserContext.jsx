ed960d4478406db4bdb274f4d24225788bb050ba
Amanda <amanda.ausejo@gmail.com>
2024-03-18 10:11:43 +0100

    fix userContex

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -2,43 +2,38 @@ import { createContext, useEffect, useReducer } from "react";
 import Reducer from "./Reducer.js";
 
 function isValidJSON(str) {
-    try {
-        JSON.parse(str);
-        return true;
-    } catch (e) {
-        return false;
-    }
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 const storedData = localStorage.getItem("data");
-const FIRST_STATE = {
-    user: null,
-    token: null,
+let FIRST_STATE = {
+  user: null,
+  token: null,
 };
 
 if (isValidJSON(storedData)) {
-    const parsedData = JSON.parse(storedData);
-    FIRST_STATE.user = parsedData.user || null;
-    FIRST_STATE.token = parsedData.token || null;
+  const parsedData = JSON.parse(storedData);
+  if (parsedData !== null && typeof parsedData === "object") {
+    FIRST_STATE = {
+      user: parsedData.user || FIRST_STATE.user,
+      token: parsedData.token || FIRST_STATE.token,
+    };
+  }
 }
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(Reducer, FIRST_STATE);
+  const [state, dispatch] = useReducer(Reducer, FIRST_STATE);
 
-    useEffect(() => {
-        localStorage.setItem(
-            "data",
-            JSON.stringify({ user: state.user, token: state.token })
-        );
-    }, [state.user, state.token]);
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify({ user: state.user, token: state.token }));
+  }, [state.user, state.token]);
 
-    console.log("UserContex state:", state);
+  console.log("UserContex state:", state);
 
-    return (
-        <Context.Provider
-            value={{ user: state.user, token: state.token, dispatch }}
-        >
-            {children}
-        </Context.Provider>
-    );
+  return <Context.Provider value={{ user: state.user, token: state.token, dispatch }}>{children}</Context.Provider>;
 };
